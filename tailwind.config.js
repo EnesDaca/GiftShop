b34f0a331fc06f213ffe80b11a40f7fc409d6c2b
EnesDaca <enesdaca@hotmail.com>
2023-04-03 22:09:52 +0200

    GiftShop Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dark: "#414143",
+        tirkiz: "#4ad9c3",
+      }
+    },
+  },
+  plugins: [],
+}
