ff98255f5dea6d6adb48cf39f46bfbe8cb6e9ae8
https://github.com/EnesDaca <enesdaca@hotmail.com>
2023-04-04 23:42:39 +0200

    UI changes2

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useTitle } from "../hooks/useTitle";
-import giftshop from "../../../assets/giftshop.png"
 
 export const PageNotFound = () => {
     useTitle("Page Not Found");
@@ -10,9 +9,6 @@ export const PageNotFound = () => {
         <section className="flex flex-col justify-center px-2">
             <div className="flex flex-col items-center my-4">
                 <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white text-center">404, Oops!</p>
-                <div className="max-w-xs">
-                    <img className="" src={giftshop} alt="Gift Shop Page Not Found" />
-                </div>
             </div>
             <div className="flex justify-center my-4">
                 <Link to="/">
